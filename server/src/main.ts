0d3c20210e940e3c2646bab9e1fd76a0d339f683
Aleksandr <49415485+whiskers888@users.noreply.github.com>
2024-02-27 20:25:35 +0300

    Подготовка к template

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,26 +4,27 @@ import { join } from 'path';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app/app.module';
-import * as cors from 'cors';
+// import * as cors from 'cors';
 
 async function startup() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    cors: false,
-  });
+	const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+		cors: false
+	});
 
-  app.use(
-    cors({
-      credentials: true,
-      origin: true,
-    }),
-  );
+	// app.use(
+	// 	cors({
+	// 		credentials: true,
+	// 		origin: true
+	// 	})
+	// );
 
-  const config = app.get(ConfigService);
-  const port = config.get<number>('SERVER_PORT');
-  app.useStaticAssets(join(__dirname, '..', 'build', 'static'));
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  app.setGlobalPrefix('api');
-  await app.listen(port);
+	const config = app.get(ConfigService);
+
+	const port = config.get<number>('SERVER_PORT');
+	app.useStaticAssets(join(__dirname, '..', 'build', 'static'));
+	app.useGlobalPipes(new ValidationPipe({ transform: true }));
+	app.setGlobalPrefix('api');
+	await app.listen(port);
 }
 
 void startup();
